57c21254174c094c6c3dd7cea85409e9f8b150db
MaxTamayo18 <94872435+MaxTamayo18@users.noreply.github.com>
2021-11-22 17:20:34 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,74 @@
+const toggleTheme = document.getElementById("toggle-theme");
+const toggleIcon = document.getElementById("toggle-icon");
+const toggleText = document.getElementById("toggle-text");
+
+const toggleColors = document.getElementById("toggle-colors")
+
+const rootStyles = document.documentElement.style;
+
+toggleTheme.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    if (toggleIcon.src.includes("moon.svg")) {
+        toggleIcon.src = "assets/icons/sun.svg";
+        toggleText.textContent = "Light Mode";
+    } else {
+        toggleIcon.src = "assets/icons/moon.svg";
+        toggleText.textContent = "Dark Mode";
+    }
+});
+
+toggleColors.addEventListener("click", (e) => {
+    rootStyles.setProperty("--primary-color", e.target.dataset.color);
+})
+
+$(document).ready(function(){
+
+    $('#btnSend').click(function(){
+
+        var errores = '';
+
+        // Validado Nombre ==============================
+        if( $('#names').val() == '' ){
+            errores += '<p>Escriba un nombre</p>';
+            $('#names').css("border-bottom-color", "#F14B4B")
+        } else{
+            $('#names').css("border-bottom-color", "#d1d1d1")
+        }
+
+        // Validado Correo ==============================
+        if( $('#email').val() == '' ){
+            errores += '<p>Ingrese un correo</p>';
+            $('#email').css("border-bottom-color", "#F14B4B")
+        } else{
+            $('#email').css("border-bottom-color", "#d1d1d1")
+        }
+
+        // Validado Mensaje ==============================
+        if( $('#mensaje').val() == '' ){
+            errores += '<p>Escriba un mensaje</p>';
+            $('#mensaje').css("border-bottom-color", "#F14B4B")
+        } else{
+            $('#mensaje').css("border-bottom-color", "#d1d1d1")
+        }
+
+        // ENVIANDO MENSAJE ============================
+        if( errores == '' == false){
+            var mensajeModal = '<div class="modal_wrap">'+
+                                    '<div class="mensaje_modal">'+
+                                        '<h3>Errores encontrados</h3>'+
+                                        errores+
+                                        '<span id="btnClose">Cerrar</span>'+
+                                    '</div>'+
+                                '</div>'
+
+            $('body').append(mensajeModal);
+        }
+
+        // CERRANDO MODAL ==============================
+        $('#btnClose').click(function(){
+            $('.modal_wrap').remove();
+        });
+    });
+
+});
+
